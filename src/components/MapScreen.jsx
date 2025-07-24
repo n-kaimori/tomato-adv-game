5ea93adab742dca07f42f093ca67b2d0f4d25ac0
n-kaimori <168047256+n-kaimori@users.noreply.github.com>
2025-07-24 10:40:45 +0900

    Create MapScreen.jsx

---
+++ src/components/MapScreen.jsx
@@ -0,0 +1,12 @@
+export default function MapScreen() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2em' }}>
+      <h2>調査する場所を選ぼう</h2>
+      <ul>
+        <li>畑</li>
+        <li>市場</li>
+        <li>厨房</li>
+      </ul>
+    </div>
+  )
+}
