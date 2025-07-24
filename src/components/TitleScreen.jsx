39fb2c68fe01b1405e5884ab35475d8c7c63a930
n-kaimori <168047256+n-kaimori@users.noreply.github.com>
2025-07-24 13:36:10 +0900

    Update TitleScreen.jsx

--- src/components/TitleScreen.jsx
+++ src/components/TitleScreen.jsx
@@ -3,8 +3,10 @@ import '../style.css'
 export default function TitleScreen({ onStart }) {
   return (
     <div className="titleScreenWrap">
-      <h1 className="titleScreenWrap--imgWrap"><img src="/title.png" alt="おいしさ探偵団 ～トマトマイスターへの道～" /></h1>
-      <button className="titleScreenWrap--buttonWrap" onClick={onStart}>ゲームスタート</button>
+      <div className="titleScreenInner">
+        <h1 className="titleScreenInner--imgWrap"><img src="/title.png" alt="おいしさ探偵団 ～トマトマイスターへの道～" /></h1>
+        <button className="titleScreenInner--buttonWrap" onClick={onStart}>ゲームスタート</button>
+      </div>
     </div>
   )
 }
